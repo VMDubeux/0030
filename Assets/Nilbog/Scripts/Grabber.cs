238b339ec59cec6fa446413a054d78d495af6cf8
Victor Dubeux <adv.victordubeux@gmail.com>
2023-09-15 18:01:44 -0300

    Commit 8
    Objeto, quando parado, sobe (eixo y). Commit 5 resolvido. Refinar movimentos e corrigir o bug quando o objeto não está mais selecionado.

--- Assets/Nilbog/Scripts/Grabber.cs
+++ Assets/Nilbog/Scripts/Grabber.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEditor.VersionControl;
 using UnityEditorInternal;
 using UnityEngine;
+using System.Collections;
 using static UnityEditor.PlayerSettings;
 
 public class Grabber : MonoBehaviour
@@ -11,37 +13,17 @@ public class Grabber : MonoBehaviour
     private Vector2 _startPos;
     private Vector2 _finalPos;
     private GameObject _selectedObject;
-    private Draggable _draggable;
     private bool _fingerIsDown;
     private bool _onTarget;
-
-    /*[Header("Elements:")]
-    private Transform toDrag;
-    private Rigidbody toDragRigidbody;
-    private bool touched = false;
-    private bool dragging = false;
-    private float posX;
-    private float posY;
-    private Vector3 dist;
-    private Vector3 previousPosition;
-
-
-
-    [Header("Settup:")]
-    [SerializeField] private Camera _mainCam;
     [SerializeField] private LayerMask _layerMask;
-    [SerializeField] private string DraggingTag;*/
 
     private void Start()
     {
-        
         _fingerIsDown = false;
     }
 
     void Update()
     {
-        _draggable = GetComponent<Draggable>();
-
         if (Input.touchCount > 0)
         {
             Touch touch = Input.touches[0];
@@ -60,8 +42,16 @@ public class Grabber : MonoBehaviour
                         if (_fingerIsDown == false)
                         {
                             Debug.Log("Começou");
-                            _startPos = touch.position;
                             _fingerIsDown = true;
+                        }
+                        break;
+
+                    case TouchPhase.Stationary:
+                        if (_fingerIsDown)
+                        {
+                            if (_selectedObject.transform.position.y <= 4.0f)
+                                _selectedObject.transform.position = Vector3.Lerp(_selectedObject.transform.position, new Vector3(_selectedObject.transform.position.x, 5f, _selectedObject.transform.position.z), 0.9f * Time.deltaTime);
+                            Debug.Log("Subindo");
                         }
                         break;
 
@@ -69,14 +59,23 @@ public class Grabber : MonoBehaviour
                         if (_fingerIsDown)
                         {
                             Debug.Log("Movendo");
-                            Vector3 position = new(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, Camera.main.WorldToScreenPoint(_selectedObject.transform.position).z);
-                            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
-                            _selectedObject.transform.position = new Vector3(worldPosition.x, _selectedObject.transform.position.y, worldPosition.z);
+                            if (_selectedObject.transform.position.z <= 6.3f)
+                            {
+                                Vector3 position = new(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, Camera.main.WorldToScreenPoint(_selectedObject.transform.position).z);
+                                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(position);
+                                _selectedObject.transform.position = new Vector3(worldPosition.x, _selectedObject.transform.position.y, worldPosition.z);
+                            }
                         }
                         break;
 
                     case TouchPhase.Ended:
                         Debug.Log("Terminou");
+                        _selectedObject.GetComponent<Draggable>().ResetPosition();
+                        _fingerIsDown = false;
+                        break;
+
+                    case TouchPhase.Canceled:
+                        Debug.Log("Cancelado");
                         _selectedObject.GetComponent<Draggable>().ResetPosition();
                         _fingerIsDown = false;
                         break;
@@ -98,7 +97,7 @@ public class Grabber : MonoBehaviour
         Vector3 worldTouchPosFar = Camera.main.ScreenToWorldPoint(screenTouchPosFar);
         Vector3 worldTouchPosNear = Camera.main.ScreenToWorldPoint(screenTouchPosNear);
         RaycastHit hit;
-        Physics.Raycast(worldTouchPosNear, worldTouchPosFar - worldTouchPosNear, out hit);
+        Physics.Raycast(worldTouchPosNear, worldTouchPosFar - worldTouchPosNear, out hit, int.MaxValue, _layerMask);
 
         return hit;
     }

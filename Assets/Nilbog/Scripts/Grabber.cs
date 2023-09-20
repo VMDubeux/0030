6a62bcfabc130f2ec74ac2c37a406d519b40e550
Arnilsen Arthur <arnilsenarthur@gmail.com>
2023-09-20 20:47:20 -0300

    Commit 12

--- Assets/Nilbog/Scripts/Grabber.cs
+++ Assets/Nilbog/Scripts/Grabber.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class Grabber : MonoBehaviour
 {
+    #region Public Fields
+    [Header("SETTINGS")]
+    public float timeToBecomeStationary = 1f;
+    public LayerMask layerMask;
+    #endregion
+
+    #region Private Fields
     private GameObject _draggingObject;
     private bool _fingerIsDown;
-    [SerializeField] private LayerMask _layerMask;
+    private Vector2 _lastTouchPosition;
+    private float _stationaryTime = 0f;
+    private Camera _camera;
+    #endregion
+
+    private void OnEnable()
+    {
+        _camera = Camera.main;
+    }
 
     private void Start()
     {
@@ -17,82 +32,67 @@ public class Grabber : MonoBehaviour
         {
             Touch touch = Input.touches[0];
 
-            if (!_fingerIsDown)
-            {
-                RaycastHit hit = CastRay();
-
-                if (hit.collider != null && hit.collider.CompareTag("drag"))
-                {
-                    _draggingObject = hit.collider.gameObject;
-                    Debug.Log("Acertou o objeto");
-                }
-            }
-
             switch (touch.phase)
             {
                 case TouchPhase.Began:
+                {
+                    RaycastHit hit = CastRay();
+
+                    if (hit.collider != null && hit.collider.CompareTag("drag"))
+                        _draggingObject = hit.collider.gameObject;
+
+                    _stationaryTime = 0;
+                    break;
+                }
+                case TouchPhase.Stationary or TouchPhase.Moved:
+                {
                     if (_draggingObject != null)
                     {
-                        Debug.Log("Começou");
-                        _fingerIsDown = true;
-                    }
-                    break;
-
-                case TouchPhase.Stationary:
-                    if (_fingerIsDown)
-                    {
-                        Invoke(nameof(UpMove), 2f);
-                    }
-                    break;
-
-                case TouchPhase.Moved:
-                    if (_fingerIsDown && _draggingObject != null)
-                    {
-                        Debug.Log("Movendo");
-                        Vector3 screenTouchPos = new(touch.position.x, touch.position.y, Camera.main.WorldToScreenPoint(_draggingObject.transform.position).z);
-                        Vector3 worldTouchPos = Camera.main.ScreenToWorldPoint(screenTouchPos);
-                        float clampedZ = Mathf.Clamp(worldTouchPos.z, float.MinValue, 6.7f);
-                        _draggingObject.transform.position = new Vector3(worldTouchPos.x, _draggingObject.transform.position.y, clampedZ);
-                    }
-                    break;
-
-                case TouchPhase.Ended:
-                case TouchPhase.Canceled:
-                    if (_fingerIsDown)
-                    {
-                        Debug.Log("Terminou");
-                        _fingerIsDown = false;
-                        if (_draggingObject != null)
+                        if (_stationaryTime >= timeToBecomeStationary)
                         {
-                            _draggingObject.GetComponent<Draggable>().ResetPosition();
-                            _draggingObject = null;
+                            if (_draggingObject.transform.position.y <= 4.0f)
+                            {
+                                var position = _draggingObject.transform.position;
+                                position += new Vector3(0, Time.deltaTime * 2f, 0);
+                                _draggingObject.transform.position = position;
+                            }
                         }
+                        else
+                        {
+                            var position = _draggingObject.transform.position;
+                            Vector3 screenTouchPos = new(touch.position.x, touch.position.y, _camera.WorldToScreenPoint(position).z);
+                            Vector3 worldTouchPos = _camera.ScreenToWorldPoint(screenTouchPos);
+                            
+                            float clampedZ = Mathf.Clamp(worldTouchPos.z, float.MinValue, 6.7f);
+                            position = new Vector3(worldTouchPos.x, position.y, clampedZ);
+                            _draggingObject.transform.position = position;
+                        }
+                        
+                        if (Vector2.Distance(touch.position, _lastTouchPosition) < 5f)
+                            _stationaryTime += Time.deltaTime;
+                        else
+                            _stationaryTime = 0;
+
+                        _lastTouchPosition = touch.position;
                     }
+                    break;
+                }
+                
+                default:
+                    if(_draggingObject != null)
+                        _draggingObject.GetComponent<Draggable>().ResetPosition();
                     break;
             }
         }
     }
-
-    private void UpMove()
-    {
-        if (_draggingObject.transform.position.y <= 4.0f)
-        {
-            _draggingObject.transform.position = Vector3.Lerp(
-                _draggingObject.transform.position,
-                new Vector3(0, 5f, _draggingObject.transform.position.z),
-                0.2f * Time.fixedDeltaTime);
-            Debug.Log("Subindo");
-        }
-    }
-
+    
     private RaycastHit CastRay()
     {
-        Vector3 screenTouchPosFar = new(Input.touches[0].position.x, Input.touches[0].position.y, Camera.main.farClipPlane);
-        Vector3 screenTouchPosNear = new(Input.touches[0].position.x, Input.touches[0].position.y, Camera.main.nearClipPlane);
-        Vector3 worldTouchPosFar = Camera.main.ScreenToWorldPoint(screenTouchPosFar);
-        Vector3 worldTouchPosNear = Camera.main.ScreenToWorldPoint(screenTouchPosNear);
-        RaycastHit hit;
-        Physics.Raycast(worldTouchPosNear, worldTouchPosFar - worldTouchPosNear, out hit, float.MaxValue, _layerMask);
+        Vector3 screenTouchPosFar = new(Input.touches[0].position.x, Input.touches[0].position.y, _camera.farClipPlane);
+        Vector3 screenTouchPosNear = new(Input.touches[0].position.x, Input.touches[0].position.y, _camera.nearClipPlane);
+        Vector3 worldTouchPosFar = _camera.ScreenToWorldPoint(screenTouchPosFar);
+        Vector3 worldTouchPosNear = _camera.ScreenToWorldPoint(screenTouchPosNear);
+        Physics.Raycast(worldTouchPosNear, worldTouchPosFar - worldTouchPosNear, out RaycastHit hit, float.MaxValue, layerMask);
 
         return hit;
     }

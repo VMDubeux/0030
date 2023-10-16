19d54112c482162ed2b2c86ad80344561f436f05
Victor Dubeux <adv.victordubeux@gmail.com>
2023-10-16 18:08:26 -0300

    Commit 16
    Ajustes diversos.

--- Assets/Nilbog/Scripts/Images.cs
+++ Assets/Nilbog/Scripts/Images.cs
@@ -2,15 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ImagesState { Empty, Filled }
 public class Images : MonoBehaviour
 {
-    public Icon[] ObjImages;
-
-    private ImagesState _state;
-
-    public void SetImage() 
-    {
-        _state = ImagesState.Empty;
-    }
+   
 }
